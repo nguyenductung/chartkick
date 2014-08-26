55fdb0ae802a3e84cf6f87e5d1c97fdbdeb33cff
NgDucTung <nguyen.duc.tung@framgia.com>
2014-08-27 01:57:15 +0700

    Add Duration page

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,2 +1,14 @@
 class View < ActiveRecord::Base
+  class << self
+    def duration_by_day
+      h = {}
+      max_hash = View.group_by_day(:created_at, format: "%B %d, %Y").maximum(:duration)
+      min_hash = View.group_by_day(:created_at, format: "%B %d, %Y").minimum(:duration)
+      max_hash.each do |k, v|
+        h[["maximum", k]] = v
+        h[["minimum", k]] = min_hash[k]
+      end
+      h.chart_json
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   root 'views#index'
-  match '/total_views', to: 'views#total_views', via: :get
-  match '/views_per_day', to: 'views#views_per_day', via: :get
-  match '/average_duration', to: 'views#average_duration', via: :get
+  match '/total_views', to: 'pages#total_views', via: :get
+  match '/views_per_day', to: 'pages#views_per_day', via: :get
+  match '/duration', to: 'pages#duration', via: :get
+  match '/average_duration', to: 'pages#average_duration', via: :get
+  [:os, :browser, :country, :day].each do |label|
+    path = "views_by_#{label.to_s}"
+    match path, to: "views##{path}", via: :get
+    path = "average_duration_by_#{label.to_s}"
+    match path, to: "views##{path}", via: :get
+    path = "duration_by_#{label.to_s}"
+    match path, to: "views##{path}", via: :get
+    next if label == :day
+    path = "views_by_#{label.to_s}_per_day"
+    match path, to: "views##{path}", via: :get
+  end
   resources :views, only: :index
 end
